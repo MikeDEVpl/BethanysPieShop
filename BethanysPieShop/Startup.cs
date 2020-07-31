c548d9712ab8e1eb1e1fc966063a6b4fc4ec26cd
MikeDevPl <46839276+MikeDEVpl@users.noreply.github.com>
2020-07-31 07:27:26 +0200

    Added Database ,EF migrations

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -6,24 +6,43 @@ using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace BethanysPieShop
 {
+
     public class Startup
     {
+        private IConfiguration _config { get; }
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IPieRepository, MockPieRepository>();
-            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
+            
+            //Mock Repositories
+            //services.AddScoped<IPieRepository, MockPieRepository>();
+            //services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+            
+            //Real repositories
+            services.AddScoped<IPieRepository, PieRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            
             //AddScoped - jeden dla requestu
             //AddSingleton - one instance
             //AddTransient - ciagle nowa
 
             services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
